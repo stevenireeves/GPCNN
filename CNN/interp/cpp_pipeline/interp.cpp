05447bdfb97fadf5f38921d824a7257f0f66afc4
Steven I Reeves <steven.reeves@appzen.com>
2019-10-22 11:05:12 -0700

    Filling out C++ pipeline

---
+++ CNN/interp/cpp_pipeline/interp.cpp
@@ -0,0 +1,12 @@
+#include "interp.h" 
+
+GPinterp::GPinterp(std::vector<float> wts, std::vector<float> gam){
+	weights = wts; 
+	gamma = gam; 
+}
+
+GPinterp::interp(const std::vector<const float> img_in, std::vector<float> img_out){
+
+
+}
+

7ba4781ed72da0157abb87d988a56accf67c2d48
Steven I Reeves <steven.reeves@appzen.com>
2019-11-12 16:03:06 -0800

    Created MakeFile for creating a shared library for use in Python. The C++ function interp.cpp will serve as a C++ to C style interface for python's c-types

---
+++ interp/cpp_pipeline/interp.cpp
@@ -0,0 +1,14 @@
+/* This is the wrapper a c-style wrapper for for the GP interpolation to be used from python. */ 
+#include "GP.h"
+#include "weights.h" 
+
+void interpolate(float *img_in, float *img_out, const int upsample_ratio[], const int in_size[]){
+    const float del[2] = {1.f/float(in_size[0]), 1.f/float(in_size[1])}; 
+    std::vector<float> img1(img_in, img_in + in_size[0]*in_size[1]); 
+    weights wgts(upsample_ratio, del); 
+    const int size[3] = {in_size[0], in_size[1], 1}; 
+    GP interp(wgts, size); 
+    std::vector<float> img2(size[0]*upsample_ratio[0]*upsample_ratio[1]*size[1], 0.f);
+    interp.gray_interp(img1, img2, upsample_ratio[0], upsample_ratio[1]); 
+    img_out = img2.data();  
+}
